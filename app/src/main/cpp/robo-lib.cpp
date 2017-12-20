243981a4c7081fb1aed033a353e7f886ca902fc5
shydh <shydhryw@qq.com>
2017-12-20 16:09:18 +0800

    12.20

--- app/src/main/cpp/robo-lib.cpp
+++ app/src/main/cpp/robo-lib.cpp
@@ -3,11 +3,100 @@
 
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+#include <android/native_window_jni.h>
+#include <android/bitmap.h>
 
 #include "move_base.h"
 
 move_base::MoveBase *pmove_base = NULL;
+
+void map_show(JNIEnv *env, jobject surface,
+              cv::Mat *input_map,
+              int width, int heigth)
+{
+    ANativeWindow_Buffer nwBuffer;
+
+    LOGI("ANativeWindow_fromSurface ");
+    ANativeWindow *mANativeWindow = ANativeWindow_fromSurface(env, surface);
+
+    if (mANativeWindow == NULL)
+    {
+        LOGI("ANativeWindow_fromSurface error");
+        return;
+    }
+
+    LOGI("ANativeWindow_lock ");
+    if (0 != ANativeWindow_lock(mANativeWindow, &nwBuffer, 0))
+    {
+        LOGI("ANativeWindow_lock error");
+        return;
+    }
+
+    cv::Mat dst_map;
+    cv::resize(*input_map, dst_map, cv::Size(width, heigth));
+
+    __uint16_t *line = (__uint16_t *) nwBuffer.bits;
+
+    u_char r, g, b;
+
+    /* Process data */
+    if (line != NULL)
+    {
+        for (int i = 0, choosehNum = 0; i < heigth; i++)
+        {
+            //获得一行
+            uchar *modify = dst_map.ptr<uchar>(i);
+            //根据缩放选取行
+            if (choosehNum++ < nwBuffer.height)
+            {
+                //LOGI("nwBuffer->format == WINDOW_FORMAT_RGB_565");
+                for (int j = 0, choosewNum = 0; j < width; j++)
+                {
+                    if (nwBuffer.format == WINDOW_FORMAT_RGB_565)
+                    {
+                        r = (u_char(modify[j])) >> 3;
+                        g = (u_char(modify[j])) >> 2;
+                        b = (u_char(modify[j])) >> 3;
+                        line[choosewNum] = (r << 11) + (g << 5) + (b << 0);
+
+                        choosewNum++;
+                    }
+                }
+                line = line + nwBuffer.stride;
+            }
+        }
+        /*memcpy(nwBuffer.bits, data, size);
+        if (nwBuffer.width == nwBuffer.stride)
+        {
+            memcpy(nwBuffer.bits, data, size);
+        } else
+        {
+            for (int i = 0; i < nwBuffer.height; i++)
+            {
+                u_char *srcPointer = data + nwBuffer.width * i * 2;
+                u_char *dstPointer = ((u_char *) nwBuffer.bits) 
+                                     + nwBuffer.stride * i * 2;
+
+                memcpy(dstPointer, srcPointer, size * 2);
+            }
+        }*/
+    }
+
+    LOGI("ANativeWindow_unlockAndPost ");
+    if (0 != ANativeWindow_unlockAndPost(mANativeWindow))
+    {
+        LOGI("ANativeWindow_unlockAndPost error");
+        return;
+    }
+
+    ANativeWindow_release(mANativeWindow);
+    LOGI("ANativeWindow_release ");
+
+    return;
+}
 
 //jstring to char*
 char *jstringTostring(JNIEnv *env, jstring jstr)
@@ -32,36 +121,21 @@ char *jstringTostring(JNIEnv *env, jstring jstr)
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_bmhri_gvp_agvp_MapView_loadIMG(JNIEnv *env, jobject instance,
-                                           jstring strDir_,
-                                           jintArray src_image_,
-                                           jint w, jint h)
+                                        jstring strDir_,
+                                        jobject surface,
+                                        jint w, jint h)
 {
     // jstring 转 char*
     char *chardata = jstringTostring(env, strDir_);
     // char* 转 string
     std::string img_src = chardata;
 
-    //test_cv testCv(img_src);
-    int size = w * h;
-
     cv::Mat srcImage = cv::imread(img_src, 0);
 
     if (pmove_base == NULL)
         pmove_base = new move_base::MoveBase(srcImage);
 
-    jint *temp = env->GetIntArrayElements(src_image_, 0);
-
-    jint b;
-
-    for (int i = 0; i < size; i++)
-    {
-        b = srcImage.data[i];
-        if (b < 0)
-            b = b + 256;
-        temp[i] = (255 << 24) | (b << 16) | (b << 8) | b;
-    }
-
-    env->ReleaseIntArrayElements(src_image_, temp, JNI_OK);
+    map_show(env, surface, &srcImage, w, h);
 
     return;
 }
@@ -81,17 +155,61 @@ Java_com_bmhri_gvp_agvp_MapView_freeMoveBase(JNIEnv *env, jobject instance)
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_bmhri_gvp_agvp_MapView_initMoveBase(JNIEnv *env,
-                                                jobject /* this */,
-                                                jdoubleArray current,
-                                                jdoubleArray goal)
+                                             jobject /* this */,
+                                             jdoubleArray current,
+                                             jdoubleArray goal)
 {
     jdouble *tmp_c = env->GetDoubleArrayElements(current, 0);
     jdouble *tmp_g = env->GetDoubleArrayElements(goal, 0);
 
-    if (pmove_base)
+    if (pmove_base != NULL)
     {
         pmove_base->initCb(tmp_c, tmp_g);
         return 1;
     } else
         return 0;
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_bmhri_gvp_agvp_MapView_planMoveBase(JNIEnv *env, jobject instance)
+{
+    if (pmove_base != NULL)
+    {
+        if (pmove_base->planCb())
+            return 1;
+        else
+            return 0;
+    } else
+        return 0;
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_bmhri_gvp_agvp_MapView_loadPath(JNIEnv *env, jobject instance,
+                                         jobject surface,
+                                         jint w, jint h)
+{
+    std::vector<std::pair<u_int, u_int>> path;
+
+    if (pmove_base != NULL)
+    {
+        path = pmove_base->get_plan();
+    } else
+        return;
+
+    cv::Mat dst_map;
+
+    dst_map = pmove_base->src_map_.clone();
+
+    for(u_long i = 0; i < path.size(); i++)
+    {
+        uchar *modify = dst_map.ptr<uchar>(path[i].first);
+        modify[path[i].second] = 1;
+    }
+
+    map_show(env, surface, &dst_map, w, h);
+
+    return;
 }
